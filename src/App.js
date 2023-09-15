e1e26e8bd18c2bc0fbd5fdc2ed2f55600c13ae6d
gopikaraj33 <120173594+gopikaraj33@users.noreply.github.com>
2023-09-15 15:18:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Meal from './Components/meal';
+import './Components/style.css';
+
+const App = () => {
+  return (
+   <>
+   <Meal/>
+   </>
+  )
+}
+
+export default App;
